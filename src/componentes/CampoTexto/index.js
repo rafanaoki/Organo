4ff7af7f943df09af6ef6f468e9b8b3e43a71d40
Rafael Naoki Oka <rafael.oka152@gmail.com>
2023-01-12 10:12:20 -0300

    Criando repositorio

---
+++ src/componentes/CampoTexto/index.js
@@ -0,0 +1,17 @@
+import './CampoTexto.css'
+
+const CampoTexto = (props) => {     
+    return (
+        <div className="campo-texto">
+            <label>{props.label}</label>
+            <input 
+                value={props.valor} 
+                onChange={evento => props.aoAlterado(evento.target.value)} 
+                required={props.obrigatorio} 
+                placeholder={`${props.placeholder}...`}
+            />
+        </div>
+    )
+};
+
+export default CampoTexto
---
+++ src/componentes/ListaSuspensa/index.js
@@ -0,0 +1,19 @@
+import './ListaSuspensa.css'
+
+const ListaSuspensa = (props) => {
+    const aoDigitado = (evento) => {
+        props.aoAlterado(evento.target.value);
+    }
+    return (
+        <div className='lista-suspensa'>
+            <label>{props.label}</label>
+            <select onChange={aoDigitado} required={props.obrigatorio} value={props.value}>
+                {props.itens.map(item => {
+                    return <option key={item}>{item}</option>
+                })}
+            </select>
+        </div>
+    )
+}
+
+export default ListaSuspensa
